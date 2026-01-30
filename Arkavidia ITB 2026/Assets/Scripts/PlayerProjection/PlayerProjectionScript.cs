118df5382a49189bd6d18b8ac14249823455a7f4
astranot09 <anthony.setiawan002@binus.ac.id>
2026-01-30 17:24:06 +0700

    projection

--- Arkavidia ITB 2026/Assets/Scripts/PlayerProjection/PlayerProjectionScript.cs
+++ Arkavidia ITB 2026/Assets/Scripts/PlayerProjection/PlayerProjectionScript.cs
@@ -18,7 +18,7 @@ public class PlayerProjectionScript : MonoBehaviour
 
     [SerializeField] private float speed;
     [SerializeField] private float currSpeed;
-    [SerializeField] private Vector2 dir = Vector2.right;
+    private Vector2 dir = Vector2.right;
 
     [SerializeField] private float jumpForce;
 
@@ -29,27 +29,32 @@ public class PlayerProjectionScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(dir.x * currSpeed, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(dir.x * currSpeed, rb.linearVelocity.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("ProjectionReverse"))
+        if (collision.CompareTag("ProjectionReverse"))
         {
-            dir *= -1;
+            dir = -dir;
+            Debug.Log("Reverse, dir = " + dir.x);
+            collision.enabled = false; // biar cuma sekali
         }
         if (collision.CompareTag("ProjectionJump"))
         {
+            Debug.Log("Jump");
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
         }
         if (collision.CompareTag("ProjectionStop"))
         {
+            Debug.Log("Stop");
             currSpeed = 0;
         }
         if (collision.CompareTag("ProjectionMove"))
         {
+            Debug.Log("Move");
             currSpeed = speed;
         }
     }
